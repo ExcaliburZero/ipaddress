de7842014fd4fbba7a905eb7ded6f82630ba9c73
ExcaliburZero <cwellsny@nycap.rr.com>
2015-11-03 13:27:38 -0500

    Added licensing info to ipaddress file
    Added in licensing info to the ipaddress file as a comment at the beginning of the file.
    
    Also made a few small style fixes.

--- ipaddress.py
+++ ipaddress.py
@@ -1,4 +1,28 @@
-"""A script which finds the ip address of the user using the /sbin/ifconfig command"""
+### BEGIN LICENSE
+# The MIT License (MIT)
+#
+# Copyright (C) 2015 Christopher Wells <cwellsny@nycap.rr.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+### END LICENSE
+"""A script which finds the ip address of the user using the /sbin/ifconfig
+ command"""
 from __future__ import print_function
 import sys
 from subprocess import Popen, PIPE
@@ -9,7 +33,7 @@ def main():
 	try:
 		# Create a terminal and get network information using /sbin/ifconfig
 		terminal = Popen("/sbin/ifconfig", shell=True, stdin=PIPE, stdout=PIPE)
-		
+
 		# Get the output from the terminal command
 		ifconfig_out = terminal.communicate()
 
